bd5e5591676e3a42c924fbf461eb21e37686a592
Rongcui Dong <carldong76@gmail.com>
2013-01-20 21:26:03 +0800

    Added health constant module.

---
+++ Character_System/HealthSystem/HPSystemConstants.py
@@ -0,0 +1,31 @@
+'''
+Created on Jan 20, 2013
+
+This module is used to store constants for HP System
+@author: carl
+'''
+
+# ---Start---: HP System Critical Points
+
+# HP System Critical Point 1, above which solid HP automatically
+# recovers.
+HP_CRITICALPOINT_1 = 0.95
+# HP System Critical Point 2. Between 1 and 2 solid HP does not 
+# change.
+HP_CRITICALPOINT_2 = 0.90
+# HP System Critical Point 3. Between 2 and 3 solid HP decreases
+# at constant rate.
+HP_CRITICALPOINT_3 = 0.10
+
+# ---End---: HP System Critical Points
+
+# ---Start---: Change rates
+
+# HP increase rate when solid HP above CP 1, points/min
+HP_INCREASE_RATE = 5.0
+# HP decrease rate when solid HP below CP 2, points/min
+HP_DECREASE_RATE = 2.0
+# HP decrease rate when solid HP below CP 3, points/min
+HP_DECREASE_RATE_2 = 20.0
+
+# ---End---: Change rates
--- Character_System/HealthSystem/HealthModule.py
+++ Character_System/HealthSystem/HealthModule.py
@@ -1,29 +1,30 @@
 '''
 Created on Jan 19, 2013
 
+HealthSystem class is the health point calculation system. It includes
+maximum health for each character, the solid and dynamic HP(Solid
+HP does not change if it is higher than critical point 2 and lower than 
+critical point 1, but 
+dynamic health drops regardless the total point is). Operation to
+hurt and recover, the function to get health. Critical points(CP) marks
+different behaviour of health: above CP1 solid health recovers automatically
+, between CP1 and 2 health remains same; between 2 and 3 health drops
+at rate 1; below 3 health drops at rate 2.
+
 @author: carl
 '''
 
-class HealthSystem:
-    '''
-    This class is the health point calculation system. It includes
-    maximum health for each character, the solid and dynamic HP(Solid
-    HP does not change if it is higher than critical point 2 and lower than 
-    critical point 1, but 
-    dynamic health drops regardless the total point is). Operation to
-    hurt and recover, the function to get health. Critical points(CP) marks
-    different behaviour of health: above CP1 solid health recovers automatically
-    , between CP1 and 2 health remains same; between 2 and 3 health drops
-    at rate 1; below 3 health drops at rate 2.
-    '''
+from HPSystemConstants import *
 
+class HealthSystem:
+    '''Health System main class'''
+    
 
     def __init__(self, MaxHP):
         '''
         Constructor: Creates a new health system
         @param MaxHP: The maximum HP
         '''
-        # TODO: Add critical points
         # Initialize maximum HP
         self.Maximum = int(MaxHP)
         # Initialize current HP
@@ -41,6 +42,7 @@ class HealthSystem:
         '''
         Returns current dynamic HP
         '''
+        return self.currentDynHP
     
     def hurt(self, damage):
         '''
@@ -58,7 +60,6 @@ class HealthSystem:
     def recover(self, recovery):
         '''
         Increase dynamic HP by recovery which is pre-calculated.
-        Can only recover 80% of total damage on solid HP.
         ''' 
         # Maximum HP after recovery, which is
         # current solid HP + 80% of total solid HP damage
