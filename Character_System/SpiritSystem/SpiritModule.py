d9f72a7bcfd9aec059b804fa0808bd14c3272a1a
Rongcui Dong <carldong76@gmail.com>
2013-01-21 07:48:05 +0800

    Start on creating hubs of spirit module on feature-MP

--- Character_System/SpiritSystem/SpiritModule.py
+++ Character_System/SpiritSystem/SpiritModule.py
@@ -16,12 +16,28 @@ class SpiritSystem():
     '''
 
 
-    def __init__(self, MaxSP):
+    def __init__(self, MaxMP):
         '''
         Constructor: Creates a new MP system.
+        @param MaxMP: The maximum MP
         '''
+        # Set maximum MP
+        self.Maximum = int(MaxMP)
+        # Initialize current MP
+        self.currentMP = self.Maximum
     #TODO: get
+    def get(self):
+        '''Returns current MP'''
+        return self.currentMP
     #TODO: hurt
+    def hurt(self, damage):
+        '''
+        Decreases MP by damage, returns if still alive
+        @param damage: Amount of damage
+        @return: If alive, return true
+        '''
+        self.currentMP -= damage
+        return self.currentMP > 0
     #TODO: decrease rates
     #TODO: recover
     #TODO: critical points
