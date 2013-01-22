0f2ac5e8b7745ebd5361de7dbe4f2ffd8c48b68d
Rongcui Dong <carldong76@gmail.com>
2013-01-22 11:57:36 +0800

    Finished TODOs in strength module

--- Character_System/StrengthSystem/StrengthModule.py
+++ Character_System/StrengthSystem/StrengthModule.py
@@ -22,9 +22,54 @@ class StrengthSystem():
         Constructor: Creates a new strength system.
         @param MaxSP: Maximum strength point
         '''
-        self.Maximum = MaxSP
+        # Initialize Max SP with cast
+        self.Maximum = float(MaxSP)
+        # Initialize current max SP
+        self.currentMax = Maximum
+        # Initialize current SP
+        self.currentSP = Maximum
+        # Initialize action multiplier
+        self.actMulti = 1.0
     
-    #TODO: weaken
-    #TODO: recover
-    #TODO: ChangeMaximum
-    #TODO: actMulti
+    def get(self):
+        '''
+        Get SP
+        '''
+        return self.currentSP
+    
+    def getMax(self):
+        '''
+        Get current max SP
+        '''
+        return self.currentMax
+    
+    def weaken(self, damage):
+        '''
+        Weaken strength by damage, return if still able to move
+        @return: If strength > 0, return true
+        '''
+        self.currentSP -= damage
+        return self.currentSP > 0
+    
+    def recover(self, recovery):
+        '''
+        Recover strength by recovery, if more than current max, set to max
+        '''
+        self.currentSP += recovery
+        if self.currentSP > self.currentMax:
+            self.currentSP = self.currentMax
+    
+    def setMax(self, newMax):
+        '''
+        Set current Max to newMax.
+        '''
+        self.currentMax = newMax
+    
+    def getActMulti(self):
+        '''
+        Gets action multiplier. If current max > max, return 1.0
+        '''
+        if self.currentMax < self.Maximum:
+            return self.currentMax / self.Maximum
+        else:
+            return 1.0
